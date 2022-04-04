0a428f6212bae360dd37b68a870203a87e6ec2d4
lihiopp <93098326+lihiopp@users.noreply.github.com>
2022-04-04 16:09:08 +0300

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,9 +1,10 @@
-import socket, threading, re, random
+import socket, threading, re, random, os
 from urllib import response
 import pandas as pd
 
 class Server:
     def __init__(self,ip,port):
+        # folders: users folders: their signature & files, the database csv file, email verification folder,  
         self.socket = socket.socket()
         self.socket.bind((ip,port))
         self.socket.listen(5)
@@ -13,8 +14,8 @@ class Server:
     
     def get_connection(self):
         client, addr = self.socket.accept()
-        t = threading.Thread(target=Server.handle_client , args=(self,client,client_id,))
         client_id = len(self.clients)
+        t = threading.Thread(target=Server.handle_client , args=(self,client,client_id,))
         self.clients.update({client_id:client})
 
     def handle_client(self,client,client_id):
@@ -24,7 +25,8 @@ class Server:
         if(action=="Signup"):
             Server.signup(self,client)
 
-        status = client.recv(1024).decode() # is the user an uploader / signer
+        # is the user an uploader / signer
+        status = client.recv(1024).decode()
         if(status=="uploader"):
             Server.uploader(self, client, username)
         if(status=="signer"):
@@ -34,11 +36,14 @@ class Server:
     def login(self, client,client_id):
         username = client.recv(1024).decode()
         password = client.recv(1024).decode()
-        while(self.df[username]!=password): ######
-            client.send("Wrong username or password. Try again.".decode())
+        while(username not in self.df['username']): ######
+            client.send("Wrong username. Try again.".decode())
             username = client.recv(1024).decode()
+        tmp = self.df.loc[['username'], ['password']]
+        while(password not in tmp['password']):
+            client.send("Wrong username. Try again.".decode())
             password = client.recv(1024).decode()
-        
+            
         # change id to username in clients dictionary
         self.clients[username] = self.clients[client_id]
         del self.clients[client_id]
@@ -47,12 +52,17 @@ class Server:
         print(str(username) + " has logged in.")
         return username
 
+
     def signup(self, client):
         username = client.recv(1024).decode()
         while(username in self.df['username']):
             client.send("100".encode()) # Username already exists
             username = client.recv(1024).decode()
         client.send("200".encode()) # Username accepted
+        
+        # creates a folder for the new user with their signature and files.
+        path = "\\users\\ " + username
+        os.mkdir(path)
         
         password = client.recv(1024).decode()
         while(len(password) < 6 ):
@@ -66,43 +76,51 @@ class Server:
             client.send("100".encode()) # Not in email format
             email = client.recv(1024).decode()
         client.send("200".encode()) # Email accepted
-        # ADD EMAIL VERIFICATION!!!
-        #signature = # Get png file and name it after username!!!
-        # Enter to database!!!
-        
+        Server.email_verification()
+        # signature = # Get png file and name it after username!!!
+        # Enter details to database!!!
+
+
     def email_verification():
+        # check email verification python file
         pass
 
-    def uploader(self,client,username):
+    def uploader(self, client, username):
         requested_signer = client.recv(1024).decode()
-        if(requested_signer in self.clients): # requested user is connected to the system
+        # if requested user is connected to the system
+        if(requested_signer in self.clients):
             message = username + " wants you to sign a PDF file. Do you accept?"
             self.clients[requested_signer].send(message.encode()) 
             response = self.clients[requested_signer].recv(1024).decode()
+            
+            # if signer accepts, the uploader sends the server the PDF file.
             if(response == "Yes"):
-                client.send("request accepted".encode()) # clients sends his file name in response
+                client.send("request accepted".encode())
                 filename = client.recv(1024).decode()
-                with open (filename, 'rb') as f:
+                path = r"C:\\Users\\idd\\Desktop\\Michals\\signatural\\users\\" + username + "\\" + filename
+                with open(path,'wb') as f:
+                    while True:
+                        data = self.client_soc.recv(1024)
+                        if not data:
+                            break
+                        f.write(data)
+                print("Got file: " + filename + " !")
+
+                # the server sends the file to the signer.
+                self.clients[requested_signer].send(filename.encode())
+                with open (path, 'rb') as f:
                     while True:
                         data=f.read(1024)
                         if not data:
                             break
-                        client.send(data)
+                        self.clients[requested_signer].send(data.encode())
+                print("Sent file: " + filename + " to signer user!")
             else:
                 client.send("request denied".encode())
                 
         else:
             client.send("User not connected.".encode())
-            
-        # get PDF file
-        filename = self.client_soc.recv(1024).decode()
-        with open(filename,'wb') as f:
-            while True:
-                data = self.client_soc.recv(1024)
-                if not data:
-                    break
-                f.write(data)
-        print("Got file: " + filename + " !")
+        
     
 def main():
     server = Server("127.0.0.1",12345)
