0b54e67e321ecf49c1b39841078e32983ed668be
lihiopp <93098326+lihiopp@users.noreply.github.com>
2022-05-13 02:06:28 +0300

    Fixed
    Working properly.

--- email_verification.py
+++ email_verification.py
@@ -1,56 +1,35 @@
-import os
-import sys
-from email.message import EmailMessage
-from email.utils import make_msgid
-import mimetypes
+import os, sys, random
 import smtplib
-from captcha.image import ImageCaptcha
-import string    
-import random
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-def send_email(username, emailaddr):
-    ''' Creates a verification email and sends it to the registered user' gmail account.'''
-    # create the mail intself.
-    msg = EmailMessage()
-    msg['Subject'] = 'Verify Your Account!'
-    msg['From'] = 'Signatural Team <signaturalteam@gmail.com>'
-    msg['To'] = '{name} <{mail}>'.format(name = username, mail = emailaddr)
-    msg.set_content('Enter the code in the required place to activate your account: 12345')
+def send(email):
+    mail_content = '''Hello,
+Thank you for joining us! We are more than happy to welcome you to our family :)
+Just one more thing! Please enter the following code to the text box in our app,
+so that we can verify that it is you and activate your account.
+If your do not know what we are talking about, please ignore this email. Thank you!"
+Code: '''
+    code = str(random.randint(0, 3000))
+    mail_content += code
+    #The mail addresses and password
+    sender_address = 'signaturalteam@gmail.com'
+    sender_pass = '326195542'
+    receiver_address = email
+    #Setup the MIME
+    message = MIMEMultipart()
+    message['From'] = sender_address
+    message['To'] = receiver_address
+    message['Subject'] = 'Verify your email address and account!'   #The subject line
+    #The body and the attachments for the mail
+    message.attach(MIMEText(mail_content, 'plain'))
+    #Create SMTP session for sending the mail
+    session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
+    session.starttls() #enable security
+    session.login(sender_address, sender_pass) #login with mail_id and password
+    text = message.as_string()
+    session.sendmail(sender_address, receiver_address, text)
+    session.quit()
+    print(code)
 
-    # creates a png of random characters - the verification code
-    try:
-        smtpserver = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-        smtpserver.ehlo()
-        smtpserver.login("signaturalteam", "zurwyvbisqaptulk") # special password to enable python to send mails.
-        smtpserver.sendmail("Signatural Team <signaturalteam@gmail.com>", "signaturalteam@gmail.com", msg.as_string())
-        smtpserver.close()
-        return 12345
-        
-    except:
-
-verification_code = ""
-def send_verify(username, emailaddr):
-    '''Sends mail and returns the required verification code.'''
-    global verification_code
-    verification_code = send_email(username, emailaddr)
-    return verification_code
-
-def check_verify(code, client):
-    '''Checks the verifiction code entered by the user, to see if it matches the one in the sent verification email.'''
-    print("Got verification code. Checking validity.")
-    input_text = code # verification code the user enters.
-    global verification_code
-    print(verification_code)
-    print("Got: ",input_text, ", Expected: ",verification_code)
-    if input_text == verification_code:
-        client.send("Valid".encode())
-    else:
-        client.send("Invalid".encode())
-
-def main(func, username, email):
-    if(sys.argv[1]=="send_verify"):
-        print(send_verify(username,email))
-    elif(sys.argv[1]=="check_verify"):
-        check_verify(code, client)
-
-main(sys.argv[1],sys.argv[2],sys.argv[3])
+send(sys.argv[1])
