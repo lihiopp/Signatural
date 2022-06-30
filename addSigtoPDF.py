4e9542f22816d44d766d49c6fcc73a12eba5ad30
lihiopp <93098326+lihiopp@users.noreply.github.com>
2022-06-30 14:50:58 +0300

    Final

--- addSigtoPDF.py
+++ addSigtoPDF.py
@@ -2,7 +2,7 @@ import fitz
 
 def start(file,signature,pageNum):
     doc = fitz.open(file)
-    rect = fitz.Rect(330, 560, 530, 764) # coordinates: x0,y0,x1,y1 (buttom right)
+    rect = fitz.Rect(330, 560, 530, 764) # coordinates: (x0, y0, x1, y1) - buttom right
     pix = fitz.Pixmap(signature)# an image file
     if(pageNum > len(doc) or pageNum==0):
         return "Failed"
